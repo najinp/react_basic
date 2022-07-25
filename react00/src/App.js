68a9716444c3c03696854769a1637dc618a57ea0
najinp <najinida92@gmail.com>
2022-07-25 12:31:50 +0900

    하하하 허

---
+++ react00/src/App.js
@@ -0,0 +1,19 @@
+import Test from "./test";
+import Nav from "./Nav";
+const App = () => {
+  //console.log('하하하')
+  return (
+    <>
+      <Test />
+      <h1>ㅎㅎ</h1>
+      <Nav />
+      <Test />
+      <Test />
+      <Test />
+      <Test />
+      {/*<h2>hahah</h2>} */}
+    </>
+
+  )
+}
+export default App;
